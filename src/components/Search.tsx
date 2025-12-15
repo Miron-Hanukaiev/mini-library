cfb2abf198389d24139ea318b57a6a7811b0bea4
Miron-Hanukaiev <kcamiron34@gmail.com>
2025-12-15 21:12:59 +0200

    - Fixing bugs

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,67 +1,78 @@
-// Component responsible for showing the sorting options
+// Component responsible for rendering search form
 
-import { BookSortProps } from '../types';
+import Filters from "./Filters";
+import Sort from "./Sort"
+import { Tag, Rating, SearchProps} from "../types";
 
-const Sort = ({
-    titleSort,
-    setTitleSort,
-    ratingSort,
-    setRatingSort,
-}: BookSortProps) => {
-    return (
-        <div className="sort-card">
+const Search = ({title, setTitle, searchBy, setSearchBy, tag, setTag, rating, setRating,
+                titleSort, setTitleSort, ratingSort, setRatingSort} : SearchProps ) => {
+
+    return ( 
+        <div className="search-book">
+            <h2>Please enter your filter:</h2>
             <form>
-                <div className="sort-section">
-                    <input
-                        type="radio"
-                        name="sort"
-                        value="A to Z"
-                        checked={titleSort === 'A to Z'}
-                        onChange={() => {
-                            setTitleSort('A to Z');
-                            setRatingSort('none');
-                        }}
-                    />
-                    <label>A→Z</label>
+                <label>Search by author/title</label>
+                <input
+                    type="text"
+                    placeholder={`Search by ${searchBy}`}
+                    value={title}
+                    onChange = {(e) => setTitle(e.target.value)} />
 
-                    <input
-                        type="radio"
-                        name="sort"
-                        value="Z to A"
-                        checked={titleSort === 'Z to A'}
-                        onChange={() => {
-                            setTitleSort('Z to A');
-                            setRatingSort('none');
-                        }}
-                    />
-                    <label>Z→A</label>
-                    <input
-                        type="radio"
-                        name="sort"
-                        value="high to low"
-                        checked={ratingSort === 'high to low'}
-                        onChange={() => {
-                            setRatingSort('high to low');
-                            setTitleSort('none');
-                        }}
-                    />
-                    <label>high→low</label>
+                <label>Select way to select</label>
+                <select id="search-by"
+                    value = {searchBy}
+                    onChange = {(e) => setSearchBy(e.target.value as "title" | "author")} >
+                    <option value="title">Title</option>
+                    <option value="author">Author</option>
+                </select>
 
-                    <input
-                        type="radio"
-                        name="sort"
-                        value="low to high"
-                        checked={ratingSort === 'low to high'}
-                        onChange={() => {
-                            setRatingSort('low to high');
-                            setTitleSort('none');
-                        }}
-                    />
-                    <label>low→high</label>
-                </div>
+                <label>Select by tag</label>
+                <select
+                    value = {tag}
+                    onChange = {(e) => setTag(e.target.value as Tag | "")} >
+                    <option value="">All Tags</option>
+                    <option value="tech">Tech</option>
+                    <option value="non-fiction">Non-Fiction</option>
+                    <option value="fiction">Fiction</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="history">History</option>
+                    <option value="self-help">Self-Help</option>
+                    <option value="science">Science</option>
+                </select>
+
+                <label>Select minimum rating (0-5)</label>
+                <input
+                    type="range"
+                    min="0"
+                    max="5"
+                    step="0.5"
+                    value={rating}
+                    onChange = {(e) => setRating(Number(e.target.value) as Rating)}>
+                </input>
+                <label>{rating} rating and above </label>
+
+
             </form>
-        </div>
-    );
-};
 
-export default Sort;
+            <Filters
+                title={title}
+                setTitle={setTitle}
+                searchBy={searchBy}
+                setSearchBy={setSearchBy}
+                tag={tag}
+                setTag={setTag}
+                rating={rating}
+                setRating={setRating} 
+            />
+
+            <Sort
+                titleSort={titleSort}
+                setTitleSort={setTitleSort}
+                ratingSort={ratingSort}
+                setRatingSort={setRatingSort}
+            />
+        </div>
+     );
+}
+ 
+export default Search;
